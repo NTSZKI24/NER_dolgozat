2b0b6dae882abe1b6f16931ebccd3866111f3dcc
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2024-11-19 14:46:47 +0100

    aa

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express"
+import dotenv from "dotenv"
+import { AppDataSource } from "./data-source"
+/* import { logger } from "./middleware/loggingMiddleware" */
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+/* app.use(logger()) */
+
+/*
+app.use("/Part", )
+app.use("/Computer", );
+app.use("/User") 
+*/
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running")
+
+    AppDataSource.initialize()
+    .then(() => {
+        console.log("Connected to database")
+    })
+})
